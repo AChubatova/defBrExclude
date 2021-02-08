dc509d5f50035c372e2af87ece176425fc41da08
admin <admin@TeamCity>
2021-02-08 15:04:41 +0100

    TeamCity change in 'git checkout rules' project: project parameters were changed

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -42,9 +42,10 @@ project {
     buildType(FilterOutDefaultBranch)
 
     params {
-        param("pwd", "pwdpwdpwdp")
         password("pwd2", "credentialsJSON:72ae78b7-3767-45b9-a0d8-2ff5ed2bf1f8")
         password("pwdtiger", "credentialsJSON:fe4ab7ed-0dc4-4bc9-8343-29c5f9e28b29")
+        param("pwd", "pwdpwdpwdp")
+        param("xcxc", "xcxc")
     }
 }
 
