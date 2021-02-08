fb7c462fa565564979e2a5bb1dcc4a5198a67627
admin <admin@TeamCity>
2021-02-08 17:53:23 +0100

    TeamCity change in 'git checkout rules' project: 'dfdf' VCS root was removed

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -29,7 +29,6 @@ version = "2020.2"
 
 project {
 
-    vcsRoot(Dfdf)
     vcsRoot(Tigernotalone)
     vcsRoot(Pirat)
     vcsRoot(Razboinik)
@@ -87,13 +86,6 @@ object Comp : GitVcsRoot({
         userName = "AChubatova"
         password = "credentialsJSON:828deb71-9d3e-47a1-a88c-d45d94705a55"
     }
-})
-
-object Dfdf : SvnVcsRoot({
-    name = "dfdf"
-    url = "https://ddlfk.visualstudor.com"
-    userName = "a"
-    password = "credentialsJSON:259ff54f-0444-4496-b247-71dc84c51dfb"
 })
 
 object Pirat : GitVcsRoot({
