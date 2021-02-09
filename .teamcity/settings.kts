5f527915e45d373836fdd77b781e4e130ba04650
admin <admin@TeamCity>
2021-02-09 12:45:53 +0100

    TeamCity change in 'pullrequest' project: triggers of 'pullreq' build configuration were updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -2,6 +2,7 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.*
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildFeatures.PullRequests
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildFeatures.commitStatusPublisher
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildFeatures.pullRequests
+import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
 import jetbrains.buildServer.configs.kotlin.v2019_2.vcs.GitVcsRoot
 
 /*
@@ -55,6 +56,13 @@ object Pullreq : BuildType({
 
     vcs {
         root(Sdsdafjjhkjh)
+    }
+
+    triggers {
+        vcs {
+            triggerRules = "+:root=${Sdsdafjjhkjh.id}:sedfjhk"
+
+        }
     }
 
     features {
