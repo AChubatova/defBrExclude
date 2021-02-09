25248f3ebb803a3b2726ba3e3dbce4cc5894b065
admin <admin@TeamCity>
2021-02-09 13:28:20 +0100

    TeamCity change in 'git checkout rules' project: 'sdpasodipoi' VCS root was created

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,4 +1,6 @@
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.powerShell
+import jetbrains.buildServer.configs.kotlin.v2019_2.vcs.GitVcsRoot
 import jetbrains.buildServer.configs.kotlin.v2019_2.vcs.SvnVcsRoot
 
 /*
@@ -27,12 +29,106 @@ version = "2020.2"
 
 project {
 
-    vcsRoot(Subv)
+    vcsRoot(Flower)
+    vcsRoot(Sdpasodipoi)
+    vcsRoot(Monkey)
+    vcsRoot(Lion)
+    vcsRoot(Comp)
+    vcsRoot(Xcxc)
+    vcsRoot(Babushka)
+
+    buildType(FilterOutDefaultBranch)
+
+    params {
+        password("pwd2", "credentialsJSON:72ae78b7-3767-45b9-a0d8-2ff5ed2bf1f8")
+        password("pwdtiger", "credentialsJSON:fe4ab7ed-0dc4-4bc9-8343-29c5f9e28b29")
+        param("pwd", "pwdpwdpwdp")
+        param("xcxc", "xcxc")
+    }
 }
 
-object Subv : SvnVcsRoot({
-    name = "subv"
-    url = "https://subv.subv"
-    userName = "alskjlk"
-    password = "credentialsJSON:fb43560d-e9e9-4ece-a50d-56d0b4a3c7e1"
+object FilterOutDefaultBranch : BuildType({
+    name = "FilterOutDefaultBranch2"
+
+    vcs {
+        root(Comp)
+
+        branchFilter = """
+            +:*
+            -:<default>
+            -:brch
+            -:branch2
+        """.trimIndent()
+    }
+
+    steps {
+        powerShell {
+            scriptMode = file {
+                path = "part4.ps1"
+            }
+            param("jetbrains_powershell_script_code", """
+                Write-output "HI"
+                Start-Sleep seconds 1
+            """.trimIndent())
+        }
+    }
+})
+
+object Babushka : SvnVcsRoot({
+    name = "babushka"
+    url = "https://sdkj.clk"
+    userName = "asdasd"
+    password = "credentialsJSON:dd2a99dd-56b8-415f-8693-c630ed71b024"
+    passphrase = "credentialsJSON:dd2c9df4-659b-44de-873c-bf0ddd8b1653"
+})
+
+object Comp : GitVcsRoot({
+    name = "comp"
+    url = "https://github.com/AChubatova/composite"
+    branch = "refs/heads/master"
+    branchSpec = """
+        +:refs/heads/(*)
+        +:mm
+    """.trimIndent()
+    authMethod = password {
+        userName = "AChubatova"
+        password = "credentialsJSON:828deb71-9d3e-47a1-a88c-d45d94705a55"
+    }
+})
+
+object Flower : SvnVcsRoot({
+    name = "flower"
+    url = "https://flower.com"
+    userName = "abc"
+    password = "credentialsJSON:3695b330-e752-4181-9fea-0f42c109abd3"
+})
+
+object Lion : SvnVcsRoot({
+    name = "lion"
+    url = "https://sdkj.dl"
+    userName = "abc"
+    password = "credentialsJSON:3695b330-e752-4181-9fea-0f42c109abd3"
+})
+
+object Monkey : SvnVcsRoot({
+    name = "monkey"
+    url = "https://skdj.bm"
+    password = "credentialsJSON:3695b330-e752-4181-9fea-0f42c109abd3"
+})
+
+object Sdpasodipoi : SvnVcsRoot({
+    name = "sdpasodipoi"
+    url = "https://doctor.do"
+    userName = "doctor"
+    password = "credentialsJSON:f8348dfe-6b69-4c9e-8d88-e1707124dd85"
+})
+
+object Xcxc : GitVcsRoot({
+    name = "xcxc"
+    url = "https://gihub.com"
+    branch = "refs/heads/master"
+    authMethod = password {
+        userName = "achubatova"
+        password = "credentialsJSON:82318fd5-6a50-409a-96a8-9cd360ddca82"
+    }
 })
